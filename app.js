c935f16c90d3b4360fb20c2ed8a9fe27d0680e37
spo1lr <choisb7366@gmail.com>
2023-04-17 22:27:59 +0900

    Chore: express 패키지 업데이트 4.16 -> 4.18

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 
 const app = express();
+app.set('port', process.env.NODE_ENV || 3000);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,5 +15,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+})
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 요청 대기 중');
+})
 
 module.exports = app;
